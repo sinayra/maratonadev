4a6081254c3849ad679fae5cfff46237dadc82b5
sinayra <nayra@hotmail.co.jp>
2020-02-18 13:18:28 -0300

    Add donors

--- server.js
+++ server.js
@@ -2,36 +2,35 @@ const express = require("express");
 const server = express();
 const nunjucks = require("nunjucks");
 
-server.listen(3000, function(){
-    console.log("init server");
-});
-
 server.use(express.static('public'));
 
+server.use(express.urlencoded({ extended: true }));
+
 nunjucks.configure("./", {
-    express: server
+    express: server,
+    noCache: true
 });
 
 
-const donors = [
-    {
-        name: "Diego Fernandes",
-        blood: "AB+"
-    },
-    {
-        name: "Diego Fernandes",
-        blood: "AB+"
-    },
-    {
-        name: "Diego Fernandes",
-        blood: "AB+"
-    },
-    {
-        name: "Diego Fernandes",
-        blood: "AB+"
-    }
-]
+const donors = []
 
 server.get("/", function(req, res) {
     return res.render("index.html", { donors });
+});
+
+server.post("/", function(req, res) {
+    const name = req.body.name;
+    const email = req.body.email;
+    const blood = req.body.blood;
+
+    donors.push({
+        name: name,
+        blood: blood
+    });
+
+    return res.redirect("/");
+});
+
+server.listen(3000, function(){
+    console.log("init server");
 });
